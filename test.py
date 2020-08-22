5189ed13abd9b6fda5eb0df0bc2b448c3faf0b38
Cx01N <anthony.rose@bc-security.org>
2020-08-22 01:05:29 -0400

    Empire updates

--- test.py
+++ test.py
@@ -5,10 +5,12 @@ from malleable.utility import MalleableError
 
 try:
     p = malleable.Profile()
-    p.ingest("./Profiles/microsoftupdate_getonly.profile")
+    p.ingest("./Profiles/gmail.profile")
     #p.ingest("putter.profile")
     if p.validate():
-        request = p.get.construct_client("mydomain.sample", "mydata")
+        request = p.get.construct_client("mydomain.sample", "mydatata")
+        print(request.url, request.headers, request.body)
+        request = p.get.construct_client("mydomain.sample", "")
         print(request.url, request.headers, request.body)
 except MalleableError as e:
     print(str(e))
